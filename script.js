91cf5c931f540f2d45a2aac751b1255d2b0c1f58
gpopowic <gmpopowich@gmail.com>
2024-02-20 13:51:48 -0800

    added localStorage function

--- script.js
+++ script.js
@@ -24,6 +24,44 @@ let draggedItem;
 let dragging = false;
 let currentColumn;
 
+// Get Arrays From LocalStorage if Available, set default values
+function getSavedColumns() {
+  if (localStorage.getItem("toDoItems")) {
+    itemListArray = JSON.parse(localStorage.toDoItems);
+    progressListArray = JSON.parse(localStorage.progressItems);
+    completeListArray = JSON.parse(localStorage.completeItems);
+    onHoldListArray = JSON.parse(localStorage.onHoldItems);
+  } else {
+    itemListArray = ["Release the course", "Sit back and relax"];
+    progressListArray = ["Work on projects", "Listen to music"];
+    completeListArray = ["ToDo list project"];
+    onHoldListArray = ["Being Cool"];
+  }
+}
+
+// Set LocalStorage Arrays
+function updateSavedColumns() {
+  listArrays = [
+    itemListArray,
+    progressListArray,
+    completeListArray,
+    onHoldListArray,
+  ];
+  const arrayNames = ["toDo", "progress", "complete", "onHold"];
+  arrayNames.forEach((arrayName, index) => {
+    localStorage.setItem(
+      `${arrayName}Items`,
+      JSON.stringify(listArrays[index])
+    );
+  });
+}
+
+// Filter Arrays to Remove Empty Items
+function filterArray(array) {
+  const filteredArray = array.filter((item) => item !== null);
+  return filteredArray;
+}
+
 // Create DOM Elements for each line
 function createItemEl(ColumnEl, column, item, index) {
   // Create list item
