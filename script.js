162525afd60dec764bef940732b6b470518d45c7
gpopowic <gmpopowich@gmail.com>
2024-02-20 13:20:45 -0800

    added global variables

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ const itemList = document.getElementById("item-list");
 const progressList = document.getElementById("progress-link");
 const completedList = document.getElementById("complete-list");
 const onHoldList = document.getElementById("on-hold-list");
+
+// Items
+let updatedOnLoad = false;
+
+// Initialize arrays
+let itemListArray = [];
+let progressListArray = [];
+let completeListArray = [];
+let onHoldListArray = [];
+let listArrays = [];
+
+// Drag Functionality
+let draggedItem;
+let dragging = false;
+let currentColumn;
